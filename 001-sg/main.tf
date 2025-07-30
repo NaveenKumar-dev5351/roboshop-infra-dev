2714613929938d043a33aaa9c0f25dffe4bda06f
naveen kumar <naveen.lingampelli21@gmail.com>
2025-07-30 14:29:16 +0530

    terraform

--- 001-sg/main.tf
+++ 001-sg/main.tf
@@ -1,8 +1,30 @@
 module "frontend" {
-    source = "../../terraform-aws-sg"
+    #source = "../../terraform-aws-sg"
+    source = "git::https://github.com/NaveenKumar-dev5351/terraform-aws-sg.git?ref=main"
     project = var.project
     environment = var.environment
+
     sg_name = var.frontend_sg_name
     sg_description = var.frontend_sg_description
     vpc_id = data.aws_ssm_parameter.vpc_id.value
+}
+
+module "bastion" {
+    #source = "../../terraform-aws-sg"
+    source = "git::https://github.com/NaveenKumar-dev5351/terraform-aws-sg.git?ref=main"
+    project = var.project
+    environment = var.environment
+
+    sg_name = var.bastion_sg_name
+    sg_description = var.bastion_sg_description
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+}
+#bastion accepting connections from my laptop
+resource "aws_security_group_rule" "bastion_laptop" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.bastion.sg_id
 }
