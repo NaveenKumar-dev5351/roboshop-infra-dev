edcf379d2f4ab029d79696543c9b69e36bf7e64b
naveen kumar <naveen.lingampelli21@gmail.com>
2025-08-09 17:37:09 +0530

    terraform

--- 60-catalogue/catalogue.sh
+++ 60-catalogue/catalogue.sh
@@ -5,4 +5,4 @@ set -e  # exit on error
 
 component=$1
 dnf install ansible -y
-ansible-pull -U https://github.com/NaveenKumar-dev5351/ansible-roboshop-roles.git -e component=$1 main.yaml
+ansible-pull -U https://github.com/NaveenKumar-dev5351/ansible-roboshop-roles-tf.git -e component=$1 -e env=$2 main.yaml
